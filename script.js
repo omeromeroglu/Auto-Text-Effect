0272be9443f2d2410c2c780534d5d640a368d61c
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-01-27 18:59:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = "We Love Programming";
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.substring(0, idx)
+    idx++
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', function (e) {
+    speed = 300 / e.target.value
+    console.log(speed)
+})
